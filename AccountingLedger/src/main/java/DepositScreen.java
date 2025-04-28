e9905c527b6261df9f15d6643e6ee226f2b3a73e
Lourie Glass <lourieglass@gmail.com>
2025-04-28 09:13:25 -0500

    Reworked deposit screen

--- AccountingLedger/src/main/java/DepositScreen.java
+++ AccountingLedger/src/main/java/DepositScreen.java
@@ -1,37 +1,63 @@
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
 import java.util.Scanner;
+import java.time.format.DateTimeFormatter;
 
 public class DepositScreen {
     public static void displayDepositScreen() {
         System.out.println("\n ----- Add a Deposit ------");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter date (YYYY-MM-DD): ");
-        String date = sc.nextLine();
-        System.out.println("Enter time (HH:MM:SS): ");
-        String time = sc.nextLine();
+
+// Set the format for date and time
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime ldt = null;
+/*Take user date and time input and ensure correct format
+        set loop to continue until valid input is received */
+        while (ldt == null) {
+            System.out.println("Enter date and time (yyyy-MM-dd HH:mm:ss): ");
+            String dateTime = sc.nextLine();
+            try {
+                ldt = LocalDateTime.parse(dateTime, dtf);
+            } catch (DateTimeParseException e) {
+                System.out.println("Invalid format. Please try again using yyyy-MM-dd HH:mm:ss");
+            }
+        }
+
+        String datePart = ldt.toLocalDate().toString();
+        String timePart = ldt.toLocalTime().toString();
+
+
         System.out.println("Enter description: ");
         String description = sc.nextLine();
         System.out.println("Enter Vendor: ");
         String vendor = sc.nextLine();
-        System.out.println("Enter amount: ");
-        double amount = sc.nextDouble();
-        sc.nextLine();
-
-
-        try (FileWriter fw = new FileWriter("transactions.csv", true);
-             PrintWriter pw = new PrintWriter(fw)) {
-            pw.println(date + "|" + time + "|" + description + "|" + vendor + "|" + amount);
-        }
-        catch (IOException e) {
-            System.out.println("Unable to add transaction" + e.getMessage());
+//Take user input and convert to a double, stay in loop until valid input received
+        double amount = 0;
+        while (true) {
+            System.out.println("Enter amount: ");
+            String amountInput = sc.nextLine();
+            try {
+                amount = Double.parseDouble(amountInput);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid amount. Please enter a valid number (e.g., 50.00");
+            }
+//Append new transactions to the file and set an exception for failure
+            try (FileWriter fw = new FileWriter("transactions.csv", true);
+                 PrintWriter pw = new PrintWriter(fw)) {
+                pw.println(datePart + "|" + timePart + "|" + description + "|" + vendor + "|" + amount);
+            } catch (IOException e) {
+                System.out.println("Unable to add transaction" + e.getMessage());
+            }
         }
     }
 
-
-
-
-
-
 }
+
+
+
+
+
